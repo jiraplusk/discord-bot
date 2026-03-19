c4bcbb8803335919f3f40b3ed17a1dd8a53ef41a
jiraplusk <jiraplusk@gmail.com>
2026-03-19 09:47:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const {
 } = require('discord.js');;
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
 });
 
 const TOKEN = process.env.TOKEN;
